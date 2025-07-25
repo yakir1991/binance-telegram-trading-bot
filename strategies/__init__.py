194aed67703117b0eae544bf02cfdb53958b4f4e
yakir1991 <95495077+yakir1991@users.noreply.github.com>
2025-07-25 15:31:06 -0700

    Create __init__.pyAdd strategies package

---
+++ strategies/__init__.py
@@ -0,0 +1,4 @@
+"""Package containing trading strategies for the Binance Telegram bot."""
+
+# This file allows Python to treat the strategies directory as a package.
+# Individual strategies are implemented in separate modules (dca, grid, scalping, etc.).
